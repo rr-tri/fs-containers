3a9775b404ac4e986f1e2e34c48533c52315a4ef
Regmiram <chry.rgm@gmail.com>
2023-12-09 22:52:17 +0545

    part 7
    Little bit of MongoDB coding
    Fixed the code.
    (GET /todos/:id) and updating one todo (PUT /todos/:id).

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -29,18 +29,22 @@ const findByIdMiddleware = async (req, res, next) => {
 
 /* DELETE todo. */
 singleRouter.delete('/', async (req, res) => {
-  await req.todo.delete()  
+  await req.todo.delete()
   res.sendStatus(200);
 });
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  res.send(req.todo); // Implement this
 });
 
 /* PUT todo. */
 singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  const { todo } = req;
+  todo.text = req.body.text;
+  todo.done = req.body.done;
+  await todo.save();
+  res.send(todo); // Implement this
 });
 
 router.use('/:id', findByIdMiddleware, singleRouter)
